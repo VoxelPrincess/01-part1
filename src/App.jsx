62c23418f34e681f8edf0f955e4e276909382d5f
VoxelPrincess <anna.v.bekharskaya@gmail.com>
2024-08-16 10:05:23 +0300

    React-projectin alkutilanne.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  console.log("Hello from komponentti");
+  return (
+    <>
+      <section>
+        <p>App-komponentissa on sisältö: Hello World!</p>
+      </section>
+    </>
+  );
+};
+
+export default App;
